24956bf5698188104b3a11b821c57e7cc108f354
Prince <prince@Princes-MacBook-Pro.local>
2018-07-24 20:58:58 -0400

    restarted

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-//testing importing forms
 import SignUp from './Components/Forms/SignUp'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        {/* <Text>Prince made a change</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text> */}
-        <SignUp /> 
+      <SignUp />
+
       </View>
     );
   }
